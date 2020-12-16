108ef594a6a7a2a77ad59f9ac5f4e535288682e2
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2020-12-16 16:00:30 +0900

    Minor update.

--- flows/utils.py
+++ flows/utils.py
@@ -15,7 +15,7 @@ def sample(n, name='moons'):
     elif name == 'normals':
         radius = 0.7
         n_normals = 8
-        k = np.random.randint(n_normals, size=(n, ))
+        k = np.random.randint(n_normals, size=(n))
         cx = radius * np.cos(2.0 * np.pi * k / n_normals)
         cy = radius * np.sin(2.0 * np.pi * k / n_normals)
         dx, dy = np.random.normal(size=(2, n)) * 0.1
@@ -33,7 +33,7 @@ def save_plot(filename, xs, ys, colors):
     fig = plt.figure(figsize=(9, 8))
     ax = fig.add_subplot(111)
     ax.set_aspect('equal', adjustable='box')
-    scatter = ax.scatter(xs, ys, c=colors, cmap='jet', vmin=0.0, vmax=15.0)
+    scatter = ax.scatter(xs, ys, c=colors, cmap='jet', vmin=0.0, vmax=5.0)
     ax.set_xlim([-1, 1])
     ax.set_ylim([-1, 1])
     plt.colorbar(scatter)
@@ -42,7 +42,7 @@ def save_plot(filename, xs, ys, colors):
 
 
 def save_image_plot(filename, image, cmap='inferno'):
-    im = plt.imshow(image, cmap='inferno', extent=[-1, 1, -1, 1], vmin=0.0, vmax=15.0)
+    im = plt.imshow(image, cmap='inferno', extent=[-1, 1, -1, 1], vmin=0.0, vmax=5.0)
     plt.colorbar(im)
     plt.savefig(filename)
     plt.close()
--- main.py
+++ main.py
@@ -12,19 +12,19 @@ from flows import Glow, Flowxx, RealNVP, InvResNet
 from flows.utils import sample, save_plot, save_image_plot
 
 networks = {
-    'realnvp': RealNVP,
-    'glow': Glow,
-    'flow++': Flowxx,
-    'iresnet': InvResNet,
+    "realnvp": RealNVP,
+    "glow": Glow,
+    "flow++": Flowxx,
+    "iresnet": InvResNet,
 }
 
 
 class Model(object):
-    def __init__(self, net='realnvp', n_dims=2, n_layers=4):
+    def __init__(self, net="realnvp", n_dims=2, n_layers=4):
         if torch.cuda.is_available():
-            self.device = torch.device('cuda', 0)
+            self.device = torch.device("cuda", 0)
         else:
-            self.device = torch.device('cpu')
+            self.device = torch.device("cpu")
 
         mu = torch.zeros(n_dims, dtype=torch.float32, device=self.device)
         covar = torch.eye(n_dims, dtype=torch.float32, device=self.device)
@@ -64,19 +64,36 @@ class Model(object):
 
 
 def main():
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.autograd.set_detect_anomaly(True)
 
     # command line arguments
-    parser = argparse.ArgumentParser(description='Flow-based generative models')
-    parser.add_argument('-n', '--network', type=str, required=True, choices=networks.keys(), help='name of network')
-    parser.add_argument('-E', '--epochs', type=int, default=20, help='training epochs')
-    parser.add_argument('-B', '--batchsize', type=int, default=1024, help='minibatch size')
-    parser.add_argument('-N', '--n_samples', type=int, default=1024, help='#samples to be drawn')
-    parser.add_argument('--dist_name',
-                        default='moons',
-                        choices=['moons', 'normals'],
-                        help='name of target distribution')
-    parser.add_argument('--output', type=str, default='outputs', help='output directory')
+    parser = argparse.ArgumentParser(description="Flow-based generative models")
+    parser.add_argument(
+        "-n",
+        "--network",
+        type=str,
+        required=True,
+        choices=networks.keys(),
+        help="name of network",
+    )
+    parser.add_argument("-E", "--epochs", type=int, default=20, help="training epochs")
+    parser.add_argument(
+        "-B", "--batchsize", type=int, default=1024, help="minibatch size"
+    )
+    parser.add_argument(
+        "-N", "--n_samples", type=int, default=1024, help="#samples to be drawn"
+    )
+    parser.add_argument(
+        "--dist_name",
+        default="moons",
+        choices=["moons", "normals"],
+        help="name of target distribution",
+    )
+    parser.add_argument(
+        "--output", type=str, default="outputs", help="output directory"
+    )
     args = parser.parse_args()
 
     # setup output directory
@@ -93,7 +110,9 @@ def main():
             y = sample(args.n_samples, name=args.dist_name)
             y = torch.tensor(y, dtype=torch.float32, requires_grad=True)
             z, loss = model.train_on_batch(y)
-            pbar.set_description('epoch #{:d}: loss={:.5f}'.format(epoch + 1, loss.item()))
+            pbar.set_description(
+                "epoch #{:d}: loss={:.5f}".format(epoch + 1, loss.item())
+            )
 
         # testing
         y, prob = model.sample(args.n_samples)
@@ -102,9 +121,9 @@ def main():
         xs = y[:, 0]
         ys = y[:, 1]
 
-        out_file = os.path.join(out_dir, 'y_sample_{:06d}.jpg'.format(epoch + 1))
+        out_file = os.path.join(out_dir, "y_sample_{:06d}.jpg".format(epoch + 1))
         save_plot(out_file, xs, ys, prob)
-        latest_file = os.path.join(out_dir, 'y_sample_latest.jpg')
+        latest_file = os.path.join(out_dir, "y_sample_latest.jpg")
         shutil.copyfile(out_file, latest_file)
 
         # 2D visualization
@@ -120,11 +139,11 @@ def main():
         prob = prob.detach().cpu().numpy()
         prob_map = prob.reshape((map_size, map_size))
 
-        out_file = os.path.join(out_dir, 'y_dist_{:06d}.jpg'.format(epoch + 1))
+        out_file = os.path.join(out_dir, "y_dist_{:06d}.jpg".format(epoch + 1))
         save_image_plot(out_file, prob_map)
-        latest_file = os.path.join(out_dir, 'y_dist_latest.jpg')
+        latest_file = os.path.join(out_dir, "y_dist_latest.jpg")
         shutil.copyfile(out_file, latest_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
